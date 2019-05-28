3e9b33e8d140af40a40c7abdde094478bb8331d6
손량 <loop.infinitely@gmail.com>
2019-05-29 01:25:15 +0900

    changed indent

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 const beatsJSON = execSync('cd ./beat-extraction && python -W ignore extractor.py song.wav').toString();
 app.use('/', express.static(__dirname + '/frontend-files'));
 app.get('/datafile', (req, res) => {
-    res.end(beatsJSON);
+  res.end(beatsJSON);
 });
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on port ${server.address.port}`);
+  console.log(`Listening on port ${server.address.port}`);
 });
